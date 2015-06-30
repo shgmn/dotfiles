712e2cf78f95e5ad98d443b588bfa08ba815d540
Takuya Shigemoto <shigemoto.takuya@gmail.com>
2015-06-30 23:32:30 +0900

    Fixed script for mac.

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -2,6 +2,6 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.vim ~/vimfiles
+ln -sf ~/dotfiles/.vim ~/.vim
 
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
